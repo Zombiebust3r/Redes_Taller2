a297c1e61776a7ce2b3510f408e871aa3e657b76
Daniel Bruna <ZOMBIEBUST3R@gmail.com>
2018-02-22 18:11:46 +0100

    Acabado
    Puede que sea necesario liberar la memoria del cliente dentro de la list<TcpSocket*> clients cuando un cliente se desconecta.

--- Taller2/main.cpp
+++ Taller2/main.cpp
@@ -8,26 +8,24 @@ void ControlServidor()
 	// Create a socket to listen to new connections
 	sf::TcpListener listener;
 	sf::Socket::Status status = listener.listen(50000);
-	if (status != sf::Socket::Done)
-	{
+	if (status != sf::Socket::Done)	{
 		std::cout << "Error al abrir listener\n";
 		exit(0);
 	}
+
 	// Create a list to store the future clients
 	std::list<sf::TcpSocket*> clients;
 	// Create a selector
 	sf::SocketSelector selector;
 	// Add the listener to the selector
 	selector.add(listener);
+
 	// Endless loop that waits for new connections
-	while (running)
-	{
+	while (running)	{
 		// Make the selector wait for data on any socket
-		if (selector.wait())
-		{
+		if (selector.wait()) {
 			// Test the listener
-			if (selector.isReady(listener))
-			{
+			if (selector.isReady(listener)) {
 				// The listener is ready: there is a pending connection
 				sf::TcpSocket* client = new sf::TcpSocket;
 				if (listener.accept(*client) == sf::Socket::Done)
@@ -46,30 +44,31 @@ void ControlServidor()
 					delete client;
 				}
 			}
-			else
-			{
+			else {	//Receive:
 				// The listener socket is not ready, test all other sockets (the clients)
-				for (std::list<sf::TcpSocket*>::iterator it = clients.begin(); it != clients.end(); ++it)
-				{
+				for (std::list<sf::TcpSocket*>::iterator it = clients.begin(); it != clients.end(); ++it) {
 					sf::TcpSocket& client = **it;
-					if (selector.isReady(client))
-					{
+					if (selector.isReady(client)) {
+						char receiveBuffer[2000];
+						std::size_t _received;
 						// The client has sent some data, we can receive it
 						sf::Packet packet;
-						status = client.receive(packet);
-						if (status == sf::Socket::Done)
-						{
+						status = client.receive(receiveBuffer, sizeof(receiveBuffer), _received);
+						if (status == sf::Socket::Done)	{
 							std::string strRec;
-							packet >> strRec;
+							strRec = receiveBuffer;
 							std::cout << "He recibido " << strRec << " del puerto " << client.getRemotePort() << std::endl;
+							//Reenviar mensaje a todos los clientes:
+							for (std::list<sf::TcpSocket*>::iterator it2 = clients.begin(); it2 != clients.end(); ++it2) {
+								sf::TcpSocket& tempSok = **it2;
+								sf::Socket::Status tempSt = tempSok.send(strRec.c_str(), strRec.length() + 1);
+							}
 						}
-						else if (status == sf::Socket::Disconnected)
-						{
+						else if (status == sf::Socket::Disconnected) {
 							selector.remove(client);
 							std::cout << "Elimino el socket que se ha desconectado\n";
 						}
-						else
-						{
+						else {
 							std::cout << "Error al recibir de " << client.getRemotePort() << std::endl;
 						}
 					}
@@ -77,7 +76,6 @@ void ControlServidor()
 			}
 		}
 	}
-
 }
 
 void main()
@@ -90,7 +88,7 @@ void main()
 	{
 		ControlServidor();
 	}
-	else if (c == 'c')
+	/*else if (c == 'c')
 	{
 		sf::TcpSocket socket;
 		sf::Socket::Status status = socket.connect("localhost", 50000, sf::milliseconds(15.f));
@@ -119,7 +117,7 @@ void main()
 		} while (str != "exit");
 		socket.disconnect();
 
-	}
+	}*/
 	else
 	{
 		exit(0);
